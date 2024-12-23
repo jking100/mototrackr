a24935f760a45487606acdedad016f4899bda370
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-23 16:50:26 -0600

    implement results viewing page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,12 +1,12 @@
+//import { useEffect, useState } from "react";
+import { AllRecordsTable } from "@/components/AllRecordsTable";
+
 export function Results() {
     return (
         <div>
-            <h1>Admin.jsx</h1>
-            <p className="text-3xl font-bold underline">
-                if this is big n bold n underlined tailwind is working
-            </p>
-            <p>The button below should be big blue and have a hover effect if daisyUI is working</p>
-            <button className="btn btn-primary w-64">Big Blue Button</button>
+            <br />
+            <h2>All Submitted Runs:</h2>
+            <AllRecordsTable />
         </div>
-    )
+    );
 }
