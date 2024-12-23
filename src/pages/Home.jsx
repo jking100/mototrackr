d9f0b33f05caaa38739b6e1a334a5f4eb6e590f3
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-23 13:39:07 -0600

    filler text for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
 export function Home() {
     return (
-        <div>
-            <h1>Home.jsx</h1>
-            <p className="text-3xl font-bold underline">
-                if this is big n bold n underlined tailwind is working
-            </p>
-            <p>The button below should be big blue and have a hover effect if daisyUI is working</p>
-            <button className="btn btn-primary w-64">Big Blue Button</button>
-        </div>
+        <>
+            <p>The app will track your 0 to 60</p>
+            <p>Click log to enter the timing screen or click view to view submitted runs from all users</p>
+            <p>This website is designed to be used only on devices with motion sensors</p>
+        </>
     )
 }
