c750afbafdc88ccd8cf6060b6c9dd66c5399e374
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-23 13:30:40 -0600

    added ssl keys to .env and added to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 
 //needed to fix definition error for __dirname
 //https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
@@ -14,5 +15,13 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(import.meta.env.VITE_SSL_KEY),
+      cert: fs.readFileSync(import.meta.env.VITE_SSL_CERT_FILE)
+    },
+    host: '0.0.0.0',
+    port: 5173
   }
 })
