d9ed1bc9cbf5a0adfdf539bb4458b0ca3fd5d887
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-23 16:50:14 -0600

    compenent to fetch all data from database and render into table

---
+++ src/components/AllRecordsTable.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { userSubmissionItemService } from "@/api";
+
+export function AllRecordsTable() {
+    const [userSubmissions, setUserSubmissions] = useState();
+
+    useEffect(() =>{
+        getData();
+    });
+
+    const tableContents = userSubmissions === undefined
+    ? <span className="loading loading-bars loading-md text-accent"></span>
+    : <div className="overflow-x-auto">
+        <table className="table table-md table-pin-rows table-pin-cols">
+            <thead>
+                <tr>
+                    <th></th>
+                    <td>Name</td>
+                    <td>Vehicle</td>
+                    <td>Time</td>
+                </tr>
+            </thead>
+            <tbody>
+                {userSubmissions.map((userSubmissions, index) =>
+                    <tr key={userSubmissions.id}>
+                        <td>{index+1}</td>
+                        <td>{userSubmissions.personName}</td>
+                        <td>{userSubmissions.vehicleName}</td>
+                        <td>{userSubmissions.zeroToSixtyTime}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    </div>;
+    
+    return (
+        <>
+            {tableContents}
+        </>
+    );
+
+    async function getData() {
+        const data = await userSubmissionItemService.getAll();
+        setUserSubmissions(data);
+    };
+}
