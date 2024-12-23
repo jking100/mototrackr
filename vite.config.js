3085434df183293415112bf053063b2c68ebe850
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-23 16:47:24 -0600

    switched from using .env ssl to using vites basic ssl plugin for the time being

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from 'path';
-import fs from 'fs';
+//import fs from 'fs';
 
 //needed to fix definition error for __dirname
 //https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
-import { fileURLToPath } from 'url'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-  server: {
-    https: {
-      key: fs.readFileSync(import.meta.env.VITE_SSL_KEY),
-      cert: fs.readFileSync(import.meta.env.VITE_SSL_CERT_FILE)
-    },
-    host: '0.0.0.0',
-    port: 5173
-  }
-})
+});
