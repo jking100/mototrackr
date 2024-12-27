ef2c3ccffa9cd1c014894a880f9a4745dc1d89e0
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-26 22:40:11 -0600

    import leanangle widget

--- src/pages/Log.jsx
+++ src/pages/Log.jsx
@@ -1,9 +1,11 @@
 import { DeviceMotionProvider } from "@/components/features/deviceMotion/";
 import { DeviceMotionTable } from "@/components/DeviceMotionTable";
+import { LeanAngleWidget } from "@/components/features/leanAngle/leanAngleWidget";
 
 export function Log() {
     return (
         <DeviceMotionProvider>
+            <LeanAngleWidget />
             <DeviceMotionTable />
         </DeviceMotionProvider>
     );
