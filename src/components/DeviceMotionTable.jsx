1a9496a70986389e36aba711ee66b345ee8a461c
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-26 22:40:44 -0600

    changed to 1 place of precision

--- src/components/DeviceMotionTable.jsx
+++ src/components/DeviceMotionTable.jsx
@@ -7,21 +7,20 @@ export function DeviceMotionTable (){
     return (
         <div className="grid gap-6">
             <br />
-                <h3>Avail.: {isAvailable ? "Yes" : "No"} | Perms: {permissionStatus ? "Yes" : "No"}</h3>
                 <div className="bg-slate-500 p-4 rounded-lg shadow">
                     <h2 className="text-lg font-semibold mb-3">Acceleration</h2>
                     <div className="grid grid-cols-3 gap-4">
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">X-Axis</div>
-                            <div className="font-mono text-gray-900 dark:text-white">{motionData.acceleration.x.toFixed(2)} m/s²</div>
+                            <div className="font-mono text-gray-900 dark:text-white">{motionData.acceleration.x.toFixed(1)} m/s²</div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Y-Axis</div>
-                            <div className="font-mono"><p>{motionData.acceleration.y.toFixed(2)} m/s²</p></div>
+                            <div className="font-mono"><p>{motionData.acceleration.y.toFixed(1)} m/s²</p></div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Z-Axis</div>
-                            <div className="font-mono"><p>{motionData.acceleration.z.toFixed(2)} m/s²</p></div>
+                            <div className="font-mono"><p>{motionData.acceleration.z.toFixed(1)} m/s²</p></div>
                         </div>
                     </div>
                 </div>
@@ -31,15 +30,15 @@ export function DeviceMotionTable (){
                     <div className="grid grid-cols-3 gap-4">
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">X-Axis</div>
-                            <div className="font-mono">{motionData.acceleration.xGrav.toFixed(2)} m/s²</div>
+                            <div className="font-mono">{motionData.acceleration.xGrav.toFixed(1)} m/s²</div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Y-Axis</div>
-                            <div className="font-mono">{motionData.acceleration.yGrav.toFixed(2)} m/s²</div>
+                            <div className="font-mono">{motionData.acceleration.yGrav.toFixed(1)} m/s²</div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Z-Axis</div>
-                            <div className="font-mono">{motionData.acceleration.zGrav.toFixed(2)} m/s²</div>
+                            <div className="font-mono">{motionData.acceleration.zGrav.toFixed(1)} m/s²</div>
                         </div>
                     </div>
                 </div>
@@ -49,15 +48,15 @@ export function DeviceMotionTable (){
                     <div className="grid grid-cols-3 gap-4">
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Alpha</div>
-                            <div className="font-mono">{motionData.rotationRate.alpha.toFixed(2)}°/s</div>
+                            <div className="font-mono">{motionData.rotationRate.alpha.toFixed(1)}°/s</div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Beta</div>
-                            <div className="font-mono">{motionData.rotationRate.beta.toFixed(2)}°/s</div>
+                            <div className="font-mono">{motionData.rotationRate.beta.toFixed(1)}°/s</div>
                         </div>
                         <div className="text-center">
                             <div className="text-sm text-gray-200 mb-1">Gamma</div>
-                            <div className="font-mono">{motionData.rotationRate.gamma.toFixed(2)}°/s</div>
+                            <div className="font-mono">{motionData.rotationRate.gamma.toFixed(1)}°/s</div>
                         </div>
                     </div>
                 </div>
