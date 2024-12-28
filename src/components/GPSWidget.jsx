2437f95539b58e975b60214c08ad0eebcd7fe487
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-27 20:09:14 -0600

    added basic functionality to test calling the gps api

--- src/components/GPSWidget.jsx
+++ src/components/GPSWidget.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+
 export function GPSWidget() {
+    const [IsGpsAvailable, setIsGpsAvailable] = useState(false);
+    const checkGPSAvailable = () => {
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+            setIsGpsAvailable(true);
+        } else {
+            /* geolocation IS NOT available */
+            setIsGpsAvailable(false);
+        }          
+    };
+
+    const logGPSInfo = (lat, long) => {
+        //i want this to insert the gps data into an array or something so i can have a table
+        //render under the button the show the result of the geolocation calls
+        alert(`(lat,long): (${lat}, ${long})`);
+    };
+
+    const getGPSInfo = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                logGPSInfo(position.coords.latitude, position.coords.longitude);
+            },
+            (fail) => {
+                alert(`Failed to get GPS info: ${fail}`);
+            },
+            {
+                enableHighAccuracy: true,
+                maximumAge: 15000,
+                timeout: 10000,
+            }
+         );
+    };
+
+    useEffect(() => {
+        checkGPSAvailable();
+    }, []);
+
     return (
-        <p>Hello from gps widget</p>
+        <>
+            <p>Hello from gps widget</p>
+            {IsGpsAvailable && (
+                <button onClick={getGPSInfo}>Get a GPS reading</button>
+            )}
+            {!IsGpsAvailable && (
+                <p>GPS NOT AVAILABLE</p>
+            )}
+
+        </>
     );
 }
