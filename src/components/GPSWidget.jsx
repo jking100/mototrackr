4db7dac53808b39d34a3170e6b075ef6c1c91738
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-27 19:50:22 -0600

    scaffolding for gps widget test component

---
+++ src/components/GPSWidget.jsx
@@ -0,0 +1,5 @@
+export function GPSWidget() {
+    return (
+        <p>Hello from gps widget</p>
+    );
+}
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,11 +2,15 @@ import { DeviceMotionProvider } from "@/components/features/deviceMotion/";
 import { DeviceMotionTable } from "@/components/DeviceMotionTable";
 import { LeanAngleWidget } from "@/components/LeanAngleWidget";
 import { LeanAngleProvider } from "@/components/features/leanAngle";
+import { GPSWidget } from "@/components/GPSWidget";
 import { useState } from "react";
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
 
 export function Test() {
     //const [view, setView] = useState("none"); //none | lean | accelerometer | 
     const [showLean, setShowLean] = useState(false);
+    const [showGPS, setShowGPS] = useState(false);
     const [showAccelerometer, setShowAccelerometer] = useState(false);
     //const [showLean, setShowLean] = useState(false);
     return (
@@ -14,11 +18,16 @@ export function Test() {
         <div style={{display: "flex", flexDirection: "column"}}>
             <input type="checkbox" id="lean" onClick={()=>{setShowLean(!showLean);}} />
             <label htmlFor="lean">Show Lean Angle widget</label>
+            <input type="checkbox" id="gps" onClick={()=>{setShowGPS(!showGPS);}} />
+            <label htmlFor="gps">Show GPS data</label>
             <input type="checkbox" id="accel" onClick={()=>{setShowAccelerometer(!showAccelerometer);}} />
             <label htmlFor="accel">Show Accelerometer data</label>
         </div>
 
         <DeviceMotionProvider>
+            {showGPS && (
+                    <GPSWidget />
+            )}
             {showLean && (
                 <LeanAngleProvider>
                     <LeanAngleWidget />
