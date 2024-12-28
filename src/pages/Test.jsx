6b34e2e46e5ac684a56ad6bccb4d50173082cdbb
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-27 19:19:23 -0600

    added buttons to toggle display of accelerometer data or lean angle

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,15 +2,32 @@ import { DeviceMotionProvider } from "@/components/features/deviceMotion/";
 import { DeviceMotionTable } from "@/components/DeviceMotionTable";
 import { LeanAngleWidget } from "@/components/LeanAngleWidget";
 import { LeanAngleProvider } from "@/components/features/leanAngle";
+import { useState } from "react";
 
 export function Test() {
+    //const [view, setView] = useState("none"); //none | lean | accelerometer | 
+    const [showLean, setShowLean] = useState(false);
+    const [showAccelerometer, setShowAccelerometer] = useState(false);
+    //const [showLean, setShowLean] = useState(false);
     return (
+        <>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <input type="checkbox" id="lean" onClick={()=>{setShowLean(!showLean);}} />
+            <label htmlFor="lean">Show Lean Angle widget</label>
+            <input type="checkbox" id="accel" onClick={()=>{setShowAccelerometer(!showAccelerometer);}} />
+            <label htmlFor="accel">Show Accelerometer data</label>
+        </div>
+
         <DeviceMotionProvider>
-            <LeanAngleProvider>
-                <LeanAngleWidget />
-            </LeanAngleProvider>
-            
-            <DeviceMotionTable />
+            {showLean && (
+                <LeanAngleProvider>
+                    <LeanAngleWidget />
+                </LeanAngleProvider>
+            )}
+            {showAccelerometer && (
+                <DeviceMotionTable />
+            )}
         </DeviceMotionProvider>
+        </>
     );
 }
