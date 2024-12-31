a76dc77b8f0fd71ecec691b7f24a798b6945442a
Joseph King <173524547+jking100@users.noreply.github.com>
2024-12-31 14:21:38 -0600

    deviceMotion provider was refactored into a useHook

--- src/components/features/deviceMotion/DeviceMotionContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-// Create a context to hold our motion data
-// null defualt since this will always be access thru a provider
-export const DeviceMotionContext = createContext(null);
