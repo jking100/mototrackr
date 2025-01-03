08a61c9a061db3838d1139060c1249d293d820b3
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-03 12:15:52 -0600

    init moto lean app view components

---
+++ src/components/motoLeanApp/AppHome.jsx
@@ -0,0 +1,8 @@
+export default function AppHome () {
+    return (
+        <div>
+            <h2>Hello from AppHome.jsx</h2>
+        </div>
+    );
+}
+
---
+++ src/components/motoLeanApp/AppInstructions.jsx
@@ -0,0 +1,8 @@
+export default function AppInstructions () {
+    return (
+        <div>
+            <h2>Hello from AppInstructions.jsx</h2>
+        </div>
+    );
+}
+
