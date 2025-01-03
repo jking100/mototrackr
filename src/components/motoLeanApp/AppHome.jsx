d80d6db85a0cde74d5e36e575758addea9ffa5f0
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-03 13:11:22 -0600

    Add navigation buttons for testing app flow

--- src/components/motoLeanApp/AppHome.jsx
+++ src/components/motoLeanApp/AppHome.jsx
@@ -1,8 +1,16 @@
-export default function AppHome () {
+import PropTypes from 'prop-types';
+
+export default function AppHome ({onStart, onViewResults}) {
     return (
         <div>
             <h2>Hello from AppHome.jsx</h2>
+            <button onClick={onStart}>Start</button>
+            <button onClick={onViewResults}>View Public Rides</button>
         </div>
     );
 }
 
+AppHome.propTypes = {
+    onStart: PropTypes.func.isRequired,
+    onViewResults: PropTypes.func.isRequired
+};
--- src/components/motoLeanApp/AppResults.jsx
+++ src/components/motoLeanApp/AppResults.jsx
@@ -1,8 +1,16 @@
-export default function AppResults () {
+import PropTypes from 'prop-types';
+
+export default function AppResults ({onHome, onSubmitRide}) {
     return (
         <div>
             <h2>Hello from AppResults.jsx</h2>
+            <button onClick={onHome}>Restart</button>
+            <button onClick={onSubmitRide}>Submit Ride</button>
         </div>
     );
 }
 
+AppResults.propTypes = {
+    onHome: PropTypes.func.isRequired,
+    onSubmitRide: PropTypes.func.isRequired
+};
