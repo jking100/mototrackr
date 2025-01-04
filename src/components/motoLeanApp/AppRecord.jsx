9c4cc7150ccb4c84dac3812dcf82946125f6a7fc
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-03 19:34:01 -0600

    changes to conditional rendering for control buttons

--- src/components/motoLeanApp/AppRecord.jsx
+++ src/components/motoLeanApp/AppRecord.jsx
@@ -31,19 +31,49 @@ export default function AppRecord ({onEndRide}) {
         <div className="p-4">
             <LeanAngleWidget motionData={Logger.motionData} />
             
+            <div className="card w-full bg-zinc-500 shadow-xl">
+                <p>Readings: {Logger.GPSReadings.length}</p>
+                <p>Elapsed Time: {Logger.GPSReadings.length * 5}s</p>
+            </div>
+            <br />
             <div className="space-x-4 mb-4">
-                <button 
-                    onClick={onEndRide}
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                    End Ride
+                {!Logger.isLogging && Logger.GPSReadings.length === 0 && (
+                    <button 
+                        onClick={() => Logger.startLogging()}
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                    >
+                        Start Logging Ride
+                    </button>
+                )}
+                
+                {Logger.isLogging && (
+                    <button 
+                        onClick={() => Logger.stopLogging()}
+                        className="bg-teal-500 text-white px-4 py-2 rounded"
+                    >
+                        Stop Logging Ride
+                    </button>
+                )}
+
+                {!Logger.isLogging && Logger.GPSReadings.length > 0 && (
+
+                    <button 
+                    onClick={() => Logger.resetGPSDataLog()}
+                    className="bg-red-500 text-white px-4 py-2 rounded"
+                    >
+                    Reset
                 </button>
-                <button 
-                    onClick={() => alert("Clicked Reset Ride")}
-                    className="bg-gray-500 text-white px-4 py-2 rounded"
-                >
-                    Reset Ride
-                </button>
+                )}
+
+                {!Logger.isLogging && Logger.GPSReadings.length > 0 && (
+                    <button 
+                        onClick={onEndRide}
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                    >
+                        View Ride Stats
+                    </button>
+                )}
+
             </div>
 
             {Logger.DiagnosticsDisplay()}
