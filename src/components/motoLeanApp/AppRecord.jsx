d1d96c77440d8e93628f6dfb18ce4dc09d1aa998
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-07 00:03:14 -0600

    removed old display components

--- src/components/motoLeanApp/AppRecord.jsx
+++ src/components/motoLeanApp/AppRecord.jsx
@@ -30,54 +30,6 @@ export default function AppRecord({ onEndRide }) {
   return (
     <>
       <GaugeDashboard onEndRide={onEndRide}/>
-      <div className="p-4">
-        <LeanAngleWidget motionData={Logger.motionData} />
-
-        <div className="card w-full bg-zinc-500 shadow-xl">
-          <p>Readings: {Logger.GPSReadings.length}</p>
-          <p>Elapsed Time: {Logger.GPSReadings.length * 5}s</p>
-        </div>
-        <br />
-        <div className="space-x-4 mb-4">
-          {!Logger.isLogging && Logger.GPSReadings.length === 0 && (
-            <button
-              onClick={() => Logger.startLogging()}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              Start Logging Ride
-            </button>
-          )}
-
-          {Logger.isLogging && (
-            <button
-              onClick={() => Logger.stopLogging()}
-              className="bg-teal-500 text-white px-4 py-2 rounded"
-            >
-              Stop Logging Ride
-            </button>
-          )}
-
-          {!Logger.isLogging && Logger.GPSReadings.length > 0 && (
-            <button
-              onClick={() => Logger.resetGPSDataLog()}
-              className="bg-red-500 text-white px-4 py-2 rounded"
-            >
-              Reset
-            </button>
-          )}
-
-          {!Logger.isLogging && Logger.GPSReadings.length > 0 && (
-            <button
-              onClick={onEndRide}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              View Ride Stats
-            </button>
-          )}
-        </div>
-
-        {Logger.DiagnosticsDisplay()}
-      </div>
     </>
   );
 }
