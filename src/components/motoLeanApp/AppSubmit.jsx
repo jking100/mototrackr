b87d108fc54b93c33500bb0434917e313ca0fb5c
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-07 00:02:33 -0600

    basic template to fill in

--- src/components/motoLeanApp/AppSubmit.jsx
+++ src/components/motoLeanApp/AppSubmit.jsx
@@ -1,16 +1,31 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function AppSubmit ({onHome, onSubmitRideToDB}) {
-    return (
-        <div>
-            <h2>Hello from AppSubmit.jsx</h2>
-            <button onClick={onHome}>Back to Home</button>
-            <button onClick={onSubmitRideToDB}>Submit Ride: Public</button>
+export default function AppSubmit({ onHome, onSubmitRideToDB }) {
+  return (
+    <div className="flex size-full justify-center items-center">
+      <div className="card portrait:w-full landscape:w-3/4 bg-base-300 m-2 max-w-3xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center mb-4">Submit Results</h2>
+          <div className="form-container mb-4">
+            <div className="min-h-[100px] bg-base-200 rounded-lg p-4">
+              Form will go here
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 sm:flex-row sm:justify-end">
+            <button className="btn btn-accent" onClick={onHome}>
+              Back to Home
+            </button>
+            <button className="btn btn-primary" onClick={onSubmitRideToDB}>
+              Submit Ride: Public
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 AppSubmit.propTypes = {
-    onHome: PropTypes.func.isRequired,
-    onSubmitRideToDB: PropTypes.func.isRequired
+  onHome: PropTypes.func.isRequired,
+  onSubmitRideToDB: PropTypes.func.isRequired,
 };
