786250b45f7981607eb3f39c14eba8b3b79037c3
Joseph King <173524547+jking100@users.noreply.github.com>
2025-01-23 18:07:22 -0600

    remove obsolete import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 //UI Components - https://daisyui.com/components/
-//import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //my pages
 import { Home } from "@/pages/Home";
-import { Test } from "@/pages/Test";
 import { Log } from "@/pages/Log";
 import { Results } from "@/pages/Results";
-
-import { GPSProvider } from "@/components/features/geolocation";
 
 function App() {
   return (
@@ -16,14 +13,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/test"
-            element={
-              <GPSProvider>
-                <Test />
-              </GPSProvider>
-            }
-          />
           <Route path="/log" element={<Log />} />
           <Route path="/results" element={<Results />} />
         </Routes>
